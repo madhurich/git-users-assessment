b726f6b083addc3bfa86c49c9c12d4eb98478e69
madhurich <madhurich23@gmail.com>
2020-12-15 20:52:51 -0600

    added http and basic template to show the rows

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'git-users-pagination';
+  repos;
+
+  constructor(private http: HttpClient) {
+    this.getUserRepos();
+  }
+
+  getUserRepos() {
+    // 1.	Repository name - name
+    // 2.	Repository description 
+    // 3.	Number of stars for the repo. stargazers_count
+    // 4.	Number of issues for the repo. open_issues_count
+    // 5.	Username and avatar of the owner. login avatar_url
+    // 6.	As a User I should be able to keep scrolling and new results should appear (pagination).
+
+    const url = `https://api.github.com/search/repositories?q=created:%3E2020-05-22&sort=stars&order=desc&page=1`;
+    return this.http.get(url)
+      .subscribe((res: any) => {
+        console.log(res);
+        this.repos = res.items;
+      })
+  }
 }
